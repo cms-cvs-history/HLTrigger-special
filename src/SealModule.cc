81c28060067e90e4c9cb90c246ae39af502c732c
cmsbuild <>
2007-04-18 16:57:37 +0000

    This commit was manufactured by cvs2git to create tag 'HLTrigger- special-V00-00-08'.
    Sprout from master 2007-04-18 16:57:36 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'Update to new L1 table.'
    Cherrypick from master 2007-04-10 23:09:24 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Adapt to new plug-in manager':
        BuildFile
        src/SealModule.cc
    Delete:
        data/isolPixelTrackFilter.cfi
        interface/HLTPixelIsolTrackFilter.h
        src/HLTPixelIsolTrackFilter.cc

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -1,9 +1,8 @@
-#include "PluginManager/ModuleDef.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "HLTrigger/special/interface/HLTPixlMBFilt.h"
-#include "HLTrigger/special/interface/HLTPixelIsolTrackFilter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTPixlMBFilt);
-DEFINE_ANOTHER_FWK_MODULE(HLTPixelIsolTrackFilter);				   
+				   
