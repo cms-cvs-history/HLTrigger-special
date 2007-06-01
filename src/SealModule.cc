004e84a4b5466a1bc720d4154a5b818408777ce1
cmsbuild <>
2007-06-01 07:03:46 +0000

    This commit was manufactured by cvs2git to create tag 'HLTrigger-special- CMSSW_1_3_1_HLT5'.
    Sprout from master 2007-05-31 14:56:04 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'Further streamlining.'
    Cherrypick from master 2007-04-10 23:09:24 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Adapt to new plug-in manager':
        BuildFile
        src/SealModule.cc
    Cherrypick from master 2007-05-31 15:05:23 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> '*** empty log message ***':
        data/SimpleBias.cff
    Cherrypick from master 2007-06-01 07:03:45 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'Fix Type.':
        data/hltPixlMBFilt.cfi
    Delete:
        data/isolPixelTrackFilter.cfi
        interface/HLTPixelIsolTrackFilter.h
        src/HLTPixelIsolTrackFilter.cc

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -2,8 +2,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "HLTrigger/special/interface/HLTPixlMBFilt.h"
-#include "HLTrigger/special/interface/HLTPixelIsolTrackFilter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTPixlMBFilt);
-DEFINE_ANOTHER_FWK_MODULE(HLTPixelIsolTrackFilter);				   
+				   
