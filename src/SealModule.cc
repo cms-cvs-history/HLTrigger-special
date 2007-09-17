ce82bd8e64146cb62bad583ffa30c2fdc0586b7b
cmsbuild <>
2007-09-17 12:40:42 +0000

    This commit was manufactured by cvs2git to create tag 'HLTrigger- special-V00-01-15'.
    Sprout from master 2007-09-17 12:40:39 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'New HLTPrescaler instance for each path.'
    Cherrypick from master 2007-05-31 15:09:11 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'Changed location.':
        BuildFile
        data/MinBias-With-Pixel-Tracks.cff
        data/SimpleBias.cff
        data/hltPixlMBFilt.cfi
        data/isolPixelTrackFilter.cfi
        src/SealModule.cc
        test/MinBias_hlt.cfg
        test/isolPixelTrackFilterTest.cfg

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -1,11 +1,9 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "HLTrigger/special/interface/HLTPixlMBFilt.h"
-#include "HLTrigger/special/interface/HLTPixlMBForAlignmentFilter.h"
 #include "HLTrigger/special/interface/HLTPixelIsolTrackFilter.h"
-#include "HLTrigger/special/interface/HLTEcalIsolationFilter.h"
 
-DEFINE_FWK_MODULE(HLTPixlMBFilt);
-DEFINE_FWK_MODULE(HLTPixlMBForAlignmentFilter);
-DEFINE_FWK_MODULE(HLTPixelIsolTrackFilter);
-DEFINE_FWK_MODULE(HLTEcalIsolationFilter);
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(HLTPixlMBFilt);
+DEFINE_ANOTHER_FWK_MODULE(HLTPixelIsolTrackFilter);				   
